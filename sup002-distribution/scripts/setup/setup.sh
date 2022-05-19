584c6c525f07ed7b1ae73d659e3cf96b85a6dd8a
Krishna Jonnalagadda <100232297+Krishnaj-jfrog@users.noreply.github.com>
2022-05-18 22:48:44 -0400

    Update setup.sh

--- sup002-distribution/scripts/setup/setup.sh
+++ sup002-distribution/scripts/setup/setup.sh
@@ -1,6 +1,22 @@
-jf rt repo-create create-npm-repo.json
-jf rt u 'npm-data/*.jar' 'sup002-swampup-maven-dev-local/hello-world/'
+#!/bin/sh
+# exit when any command fails
+set -e
+
+_user=$1
+_password=$2
+_url=$3
 
 
-jf rt repo-create create-maven-repo.json
-jf rt u 'maven-data/*.tgz' 'sup002-swampup-npm-dev-local/hello-world-ui/'
+create_validate_data 'npm' 'tgz' 'ui'
+create_validate_data 'maven' 'jar' 'api'
+
+create_validate_data(){
+    repo_name=sup002-swampup-$1-dev-local #derive from what is written in json file 
+    ext=$2
+    app_name=hello-world-$3
+    jf rt repo-create create-$type-repo.json --user $_user --password  $_password --url $_url
+    jf rt u '$1-data/$app_name-*.$ext' '$repo_name/$app_name/' --user $_user --password  $_password --url $_url
+    jf rt s $repo_name/$app_name/ --count | grep 3. # validate if artifacts are uploaded .. we know we upload 3 artifacts 
+}
+
+
