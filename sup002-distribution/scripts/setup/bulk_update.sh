d7bca792b0beb590fc9e61d14c38ec62e2342c09
Krishna Jonnalagadda <100232297+Krishnaj-jfrog@users.noreply.github.com>
2022-05-19 10:14:38 -0400

    Update bulk_update.sh

--- sup002-distribution/scripts/setup/bulk_update.sh
+++ sup002-distribution/scripts/setup/bulk_update.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+i=1
+number=9
+while [ "$i" -le "$number" ]; do
+   echo "$i"
+   sh setup.sh "taletsup002epsu0$i@jfrog.com" "SwampUp2022!" "https://sup002epsu0$i.jfrog.io/artifactory"
+   #sleep 3
+   i=$(($i + 1))
+done
 i=10
 number=30
 while [ "$i" -le "$number" ]; do
