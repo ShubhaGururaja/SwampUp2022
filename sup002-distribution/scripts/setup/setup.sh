3c84c24c1f7e0ed6b7372229ce35b98bb89a4ca1
krishna <krishnaj@jfrog.com>
2022-05-19 02:22:51 +0000

    scripts for setup

---
+++ sup002-distribution/scripts/setup/setup.sh
@@ -0,0 +1,6 @@
+jf rt repo-create create-npm-repo.json
+jf rt u 'npm-data/*.jar' 'sup002-swampup-maven-dev-local/hello-world/'
+
+
+jf rt repo-create create-maven-repo.json
+jf rt u 'maven-data/*.tgz' 'sup002-swampup-npm-dev-local/hello-world-ui/'
