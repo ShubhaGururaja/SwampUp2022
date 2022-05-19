c514a56c32d7688a725e070e06e181a2132a76b6
Krishna Jonnalagadda <100232297+Krishnaj-jfrog@users.noreply.github.com>
2022-05-19 11:52:30 -0400

    Update setup.sh

--- sup002-distribution/scripts/setup/setup.sh
+++ sup002-distribution/scripts/setup/setup.sh
@@ -12,7 +12,7 @@ create_validate_data(){
     ext=$2
     app_name=hello-world-$3
     #jf rt rdel $repo_name --user $_user --password $_password --url $_url
-    jf rt repo-create create-$1-repo.json --user $_user --password  $_password --url $_url || true
+    jf rt repo-create create-$1-repo.json --user $_user --password  $_password --url $_url || true #ignore the failures so that it gets idempotent 
     jf rt u "$app_name/*.$ext" "$repo_name/" --user $_user --password  $_password --url $_url
     jf rt s $repo_name/$app_name/ --count --user $_user --password  $_password --url $_url | grep 3. # validate if artifacts are uploaded .. we know we upload 3 artifacts 
 }
